2026aa5fd3f7297133209af77707c6ffb1012d13
osun24 <osun342@gmail.com>
2025-05-23 22:45:30 -0700

    add docs

--- src/main/java/FirebaseInitializer.java
+++ src/main/java/FirebaseInitializer.java
@@ -1,18 +1,21 @@
+// Libraries to access Firebase Realtime Database
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import java.io.IOException;
-
-import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
-import java.nio.charset.StandardCharsets;
 
 // Connect to Firebase 
+// Reference: https://firebase.google.com/docs/database/admin/start
 public class FirebaseInitializer {
+    // Initializes Firebase with the service account credentials 
     public static void initialize() throws IOException {
         if (FirebaseApp.getApps().isEmpty()) {
+            // REMOVE BEFORE GOING TO PROD
             // Use environment variable for service account JSON
             // export GOOGLE_APPLICATION_CREDENTIALS="tokens/key.json"
+
+            // Use environmental variable instead of hardcoding the path
             String serviceAccountJson = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
             if (serviceAccountJson == null || serviceAccountJson.isEmpty()) {
                 throw new IllegalStateException("GOOGLE_APPLICATION_CREDENTIALS environment variable is not set or empty.");
@@ -23,7 +26,7 @@ public class FirebaseInitializer {
             // Initialize Firebase with the service account
             FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
-                .setDatabaseUrl("https://contactsadder-ea69e-default-rtdb.firebaseio.com/")
+                .setDatabaseUrl("https://contactsadder-ea69e-default-rtdb.firebaseio.com/") // specific database URL
                 .build();
 
             FirebaseApp.initializeApp(options);
--- src/main/java/Handler.java
+++ src/main/java/Handler.java
@@ -8,41 +8,43 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.database.*;
 
-import java.io.FileInputStream;
-
 // Use countdownlatch to wait for firebase to load
+// Ensure we don't jump (due to Firebase being async) until after the people have been fetched
 import java.util.concurrent.CountDownLatch;
 
 import java.awt.Desktop;
 import java.io.FileWriter;
-import java.io.IOException;
 import com.opencsv.CSVWriter;
 
+// STATE MACHINE to handle accessing Firebase, reading CSV, and writing to Firebase
+// Backend of application
+// Written by Owen and William
 public class Handler {
     // Create an arraylist of Person objects to store the contacts as they are created/loaded
+    // Might be slightly redundant but ensures that we have a unique list of emails
     public static HashMap<String, Person> emailsToExport = new HashMap<>();
 
+    // Person objects to export to CSV (new contacts) --> import to user's Google Contacts
     public static ArrayList<Person> peopleToCSV = new ArrayList<>();
     public static ArrayList<String> databaseEmails = new ArrayList<>(); // emails in the database --> avoid duplicates
-    public static ArrayList<String> userEmails = new ArrayList<>(); // emails that the user has
+    public static ArrayList<String> userEmails = new ArrayList<>(); // emails that the user has (from uploaded CSV)
     public static ArrayList<Person> newPeople = new ArrayList<>(); // people from the person's contacts
 
     public static CountDownLatch latch = new CountDownLatch(1);
 
-    // Instantiate STATE MACHINE because the firebase in async but the file processing is sync
-    // This is annoying
+    // Instantiate our STATE MACHINE because the firebase in async but the file processing is sync 
+    // Prevents instant jumps (and prevents the program from not working)
     enum State {
         FETCH, AWAITING_FILE, PROCESSING_CSV, PUSH, DONE
     }
     public static State currentState = State.FETCH;
 
+    // File to store the CSV file that the user uploads
     static File csvFile;
 
+    // This is commented out because we added the Interface - no longer necessary
     // change from main to throws InterruptedException to let latch work
     // interrupted exception is thrown when the thread is interrupted
     // public static void main(String[] args) throws InterruptedException {
@@ -50,167 +52,183 @@ public class Handler {
     //     latch.await(); // Program keeps running until counted down
     // }
 
+    // Move between states
     public static void handleState(State state) {
-        switch (state) {
-        /**
-        * GET DATA FROM FIREBASE 
-        */
-        case FETCH:
-            System.out.println("Loading Firebase...");
+        switch (state) { 
+            /**
+             * FETCHING DATA FROM FIREBASE
+             */
+            case FETCH:
+                System.out.println("Loading Firebase...");
 
-            try {
-                FirebaseInitializer.initialize();
-            } catch (IOException e) {
-                System.out.println("Failed to initialize Firebase: " + e.getMessage());
-                return;
-            }
+                // Initialize Firebase
+                try {
+                    FirebaseInitializer.initialize();
+                } catch (IOException e) {
+                    System.out.println("Failed to initialize Firebase: " + e.getMessage());
+                    return;
+                }
 
-            // Retrieve data from Firebase Realtime Database
-            DatabaseReference ref = FirebaseDatabase.getInstance().getReference("people");
+                // Retrieve data from Firebase Realtime Database
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference("people");
+                
+                // Listener to read data from Firebase
+                ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot snapshot) {
+                        for (DataSnapshot child : snapshot.getChildren()) {
+                            Person person = child.getValue(Person.class);
+
+                            // Check if duplicate email
+                            if (databaseEmails.contains(person.getEmail())) {
+                                System.out.println("- Person with email: " + person.getEmail() + " already exists in the database.");
+                            
+                            // Check if person is valid
+                            } else if (person != null) {
+                                peopleToCSV.add(person);
+                                emailsToExport.put(person.getEmail(), person);
+                                databaseEmails.add(person.getEmail());
+                                
+                                System.out.println("+ Fetched person: " + person.getEmail() + ", " + person.getFirstName() + " " + person.getLastName());
+                            }
+                        }
+                    
+                        // Set the current state to AWAITING_FILE after fetching data
+                        currentState = State.AWAITING_FILE;
+                        handleState(currentState);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        System.err.println("Firebase read failed: " + error.getMessage());
+                    }
+                });
+                break;
             
-            ref.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot snapshot) {
-                    for (DataSnapshot child : snapshot.getChildren()) {
-                        Person person = child.getValue(Person.class);
+            /**
+            * WAITING FOR CSV UPLOAD
+            */
+            case AWAITING_FILE:
+                System.out.println("Awaiting file...");
+                // invokeLater() ensures that the file chooser actually opens
+                // without this, the file chooser might not open properly for the user
+                SwingUtilities.invokeLater(() -> {
+                    System.out.println("Opening file chooser...");
+
+                    // Prompt the user to select a CSV file via file chooser
+                    JFileChooser fileChooser = new JFileChooser();
+                    System.out.println("File chooser opened.");
+
+                    // Tell the user to select their Google Contacts CSV file
+                    fileChooser.setDialogTitle("Select your Google Contacts CSV file");
+
+                    // Restrain to csv
+                    fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                    fileChooser.setAcceptAllFileFilterUsed(false);
+                    fileChooser.addChoosableFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("CSV files", "csv"));
+
+                    // Start at home directory
+                    String userHome = System.getProperty("user.home");
+
+                    // Check if the OS is Mac to use Downloads (for greater convenience)
+                    String os = System.getProperty("os.name").toLowerCase();
+                    File initialDir;
+                    if (os.contains("mac")) {
+                        initialDir = new File(userHome, "Downloads");
+                    } else {
+                        initialDir = new File(userHome);
+                    }
+                    fileChooser.setCurrentDirectory(initialDir);
+
+                    // Set the default file name to contacts.csv
+                    fileChooser.setSelectedFile(new File("contacts.csv")); 
+
+                    // Select the file and move to process it
+                    int result = fileChooser.showOpenDialog(null);
+                    if (result == JFileChooser.APPROVE_OPTION) {
+                        csvFile = fileChooser.getSelectedFile();
+                        // Set the current state to PROCESSING_CSV after selecting the file
+                        currentState = State.PROCESSING_CSV;
+                        handleState(currentState);
+                    }
+                });
+                break;
+            
+            /**
+             * READ DATA FROM UPLOADED CSV
+             */
+            case PROCESSING_CSV:
+                System.out.println("Processing CSV...");
+
+                // Check if the csvFile is null
+                try (CSVReader reader = new CSVReader(new FileReader(csvFile))) {
+                    // Go row-by-row and create Person objects
+                    List<String[]> rows = reader.readAll();
+
+                    // Enahnced for loop - don't need to use index
+                    for (String[] row : rows) {
+                        // Create Person object from the CSV row
+                        Person person = createPersonFromCSVRow(row);
+                        
+                        // Skip if the person is null
+                        if (person == null) continue;
+
+                        // Check if the email is already in the emailsToExport map (prevent duplicates)
+                        if (emailsToExport.keySet().contains(person.getEmail())) {
+                            emailsToExport.remove(person.getEmail());
+                            System.out.println("> Person with email: " + person.getEmail() + " already exists in the user's contacts.");
+                        }
 
                         // Check if duplicate email
                         if (databaseEmails.contains(person.getEmail())) {
                             System.out.println("- Person with email: " + person.getEmail() + " already exists in the database.");
                         
-                        // Check if person is valid
-                        } else if (person != null) {
-                            peopleToCSV.add(person);
-                            emailsToExport.put(person.getEmail(), person);
+                        // Check if the email ends with @lbschools.net
+                        } else if (person.getEmail().endsWith("@lbschools.net")) {
+                            newPeople.add(person);
                             databaseEmails.add(person.getEmail());
-                            
-                            System.out.println("+ Fetched person: " + person.getEmail() + ", " + person.getFirstName() + " " + person.getLastName());
+
+                            System.out.println("+ Adding person with email: " + person.getEmail());
                         }
                     }
-                
-                    // Set the current state to AWAITING_FILE after fetching data
-                    currentState = State.AWAITING_FILE;
+
+                    // Set the current state to PUSH after processing the CSV --> push to Firebase
+                    currentState = State.PUSH;
+                    System.out.println("CSV file read successfully. Number of people: " + emailsToExport.size());
                     handleState(currentState);
+                } catch (Exception e) {
+                    System.out.println("Something went wrong while reading the CSV file.");
+                    e.printStackTrace();
+                }
+                break;
+            
+            /**
+             * UPLOADING CSV TO FIREBASE
+             */
+            case PUSH:
+                System.out.println("Uploading...");
+
+                DatabaseReference peopleFB = FirebaseDatabase.getInstance().getReference("people");
+                // Add each person to the Firebase Realtime Database
+                for (Person person : newPeople) {
+                    peopleFB.push().setValueAsync(person);
+                    System.out.println("Added person to Firebase: " + person.getEmail());
                 }
 
-                @Override
-                public void onCancelled(DatabaseError error) {
-                    System.err.println("Firebase read failed: " + error.getMessage());
-                }
-            });
-            break;
-        
-        /**
-        * WAITING FOR CSV UPLOAD
-        */
-        case AWAITING_FILE:
-            System.out.println("Awaiting file...");
-            SwingUtilities.invokeLater(() -> {
-                System.out.println("Opening file chooser...");
-
-                // Prompt the user to select a CSV file via file chooser
-                JFileChooser fileChooser = new JFileChooser();
-                System.out.println("File chooser opened.");
-
-                // Tell the user to select their Google Contacts CSV file
-                fileChooser.setDialogTitle("Select your Google Contacts CSV file");
-
-                // Restrain to csv
-                fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-                fileChooser.setAcceptAllFileFilterUsed(false);
-                fileChooser.addChoosableFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("CSV files", "csv"));
-
-                // Start at home directory
-                String userHome = System.getProperty("user.home");
-
-                // Check if the OS is Mac to use Downloads (for greater convenience)
-                String os = System.getProperty("os.name").toLowerCase();
-                File initialDir;
-                if (os.contains("mac")) {
-                    initialDir = new File(userHome, "Downloads");
-                } else {
-                    initialDir = new File(userHome);
-                }
-                fileChooser.setCurrentDirectory(initialDir);
-                fileChooser.setSelectedFile(new File("contacts.csv")); // Set default file name
-                int result = fileChooser.showOpenDialog(null);
-                if (result == JFileChooser.APPROVE_OPTION) {
-                    csvFile = fileChooser.getSelectedFile();
-                    // Set the current state to PROCESSING_CSV after selecting the file
-                    currentState = State.PROCESSING_CSV;
-                    handleState(currentState);
-                }
-                });
-
-            break;
-        
-        /**
-         * READ DATA FROM UPLOADED CSV
-         */
-        case PROCESSING_CSV:
-            System.out.println("Processing CSV...");
-
-            try (CSVReader reader = new CSVReader(new FileReader(csvFile))) {
-                List<String[]> rows = reader.readAll();
-                for (String[] row : rows) {
-                    Person person = createPersonFromCSVRow(row);
-                    
-                    // Skip if the person is null
-                    if (person == null) continue;
-
-                    if (emailsToExport.keySet().contains(person.getEmail())) {
-                        emailsToExport.remove(person.getEmail());
-                        System.out.println("> Person with email: " + person.getEmail() + " already exists in the user's contacts.");
-                    }
-
-                    // Check if duplicate email
-                    if (databaseEmails.contains(person.getEmail())) {
-                        System.out.println("- Person with email: " + person.getEmail() + " already exists in the database.");
-                    
-                    // Check if the email ends with @lbschools.net
-                    } else if (person.getEmail().endsWith("@lbschools.net")) {
-                        newPeople.add(person);
-                        databaseEmails.add(person.getEmail());
-
-                        System.out.println("+ Adding person with email: " + person.getEmail());
-                    }
-                }
-
-                // Set the current state to PROCESSING_CSV after reading the file
-                currentState = State.PUSH;
-                System.out.println("CSV file read successfully. Number of people: " + emailsToExport.size());
+                currentState = State.DONE;
                 handleState(currentState);
-            } catch (Exception e) {
-                System.out.println("Something went wrong while reading the CSV file.");
-                e.printStackTrace();
-            }
-            break;
-        
-        /**
-         * UPLOADING CSV TO FIREBASE
-         */
-        case PUSH:
-            System.out.println("Uploading...");
-            DatabaseReference peopleFB = FirebaseDatabase.getInstance().getReference("people");
-            // Add each person to the Firebase Realtime Database
-            for (Person person : newPeople) {
-                peopleFB.push().setValueAsync(person);
-                System.out.println("Added person to Firebase: " + person.getEmail());
-            }
-
-            currentState = State.DONE;
-            handleState(currentState);
-            break;
-        
-        /**
-         * CSV DOWNLOADED
-         */
-        case DONE:
-            createCSV(new ArrayList<>(emailsToExport.values()));
-            System.out.println("Done.");
-            latch.countDown();
-            break;
-    }}
+                break;
+            
+            /**
+             * CREATE CSV FILE WITH NEW CONTACTS
+             */
+            case DONE:
+                createCSV(new ArrayList<>(emailsToExport.values()));
+                System.out.println("Done.");
+                latch.countDown(); // Count down the latch to signal that the process is complete 
+                break;
+        }
+    }
 
     // Extract the contact information from the CSV file 
     // and create a Person object for each contact
@@ -238,6 +256,7 @@ public class Handler {
     }
 
     // Create a CSV file with the contacts from Firebase
+    // Following format for Google Contacts CSV
     public static void createCSV(ArrayList<Person> people) {
         // HEADER: First Name,Middle Name,Last Name,Phonetic First Name,Phonetic Middle Name,Phonetic Last Name,Name Prefix,Name Suffix,Nickname,File As,Organization Name,Organization Title,Organization Department,Birthday,Notes,Photo,Labels,E-mail 1 - Label,E-mail 1 - Value
         String[] header = {"First Name", "Middle Name", "Last Name", "Phonetic First Name", "Phonetic Middle Name", "Phonetic Last Name", "Name Prefix", "Name Suffix", "Nickname", "File As", "Organization Name", "Organization Title", "Organization Department", "Birthday", "Notes", "Photo", "Labels", "E-mail 1 - Label", "E-mail 1 - Value"};
@@ -257,7 +276,7 @@ public class Handler {
             System.out.println("Failed to create CSV file: " + e.getMessage());
         }
         System.out.println("CSV file created successfully: " + csvFile.getAbsolutePath());
-        // Open the CSV file in the default application
+        // Open the CSV file in the default application 
         try {
             Desktop.getDesktop().open(csvFile);
         } catch (IOException e) {
